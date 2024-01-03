7732a686543cb7977f0583df7f28b4526c3a2226
Jana2xN <jana.neirinck@hotmail.com>
2024-01-03 16:02:25 +0100

    Add label, input and button to the form

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
 <body>
     <h1>Currency Calculator</h1>
     <form action="" method="get">
+        <label for="amount">Enter Amount in Pounds (£):</label>
+        <input type="number" step="0.01" id="amount" name="amount" required><br><br>
+        <button type="submit">Convert to Euros</button>
     </form>
 </body>
 </html>
