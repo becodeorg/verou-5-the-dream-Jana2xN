46b17aa7969116ea4001dd2390c79cab11ee3516
Jana2xN <jana.neirinck@hotmail.com>
2024-01-03 16:09:44 +0100

    Add php

--- index.php
+++ index.php
@@ -11,5 +11,19 @@
         <input type="number" step="0.01" id="amount" name="amount" required><br><br>
         <button type="submit">Convert to Euros</button>
     </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["amount"])) {
+        $exchangeRate = 0.87; 
+        $amountInPounds = $_GET["amount"];
+
+        if (is_numeric($amountInPounds) && $amountInPounds >= 0) {
+            $amountInEuros = $amountInPounds * $exchangeRate;
+            echo "<p>{$amountInPounds} pounds is equal to {$amountInEuros} euros.</p>";
+        } else {
+            echo "<p>Please enter a valid amount.</p>";
+        }
+    }
+    ?>
 </body>
 </html>
